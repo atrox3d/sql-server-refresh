9a6c1900aad24672b009df45eff77d9421ca2704
atrox3d <atrox3d@gmail.com>
2026-01-08 14:23:13 +0100

    db commands

--- 02-create-db.sql
+++ 02-create-db.sql
@@ -2,3 +2,16 @@
 IF DB_ID('sample') IS NULL
 	CREATE DATABASE sample;
 
+-- check if there is a db 'sample'
+SELECT name, database_id, create_date
+FROM sys.databases
+WHERE name = 'sample'
+;
+
+-- rename sample to sample2 with commands
+ALTER DATABASE sample MODIFY NAME = sample2;
+
+-- rename back sample using stored procedure
+EXECUTE sp_renamedb 'sample2', 'sample';
+
+DROP DATABASE sample;
