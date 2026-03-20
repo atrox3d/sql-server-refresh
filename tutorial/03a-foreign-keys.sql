f1b89bb3bc55ec88fb672cc412b0b3d29d5aaf44
Giuseppe Roberto Lombardo <ukg15425@kireygroup.com>
2026-03-20 09:12:22 +0100

    working with fk

--- tutorial/03a-foreign-keys.sql
+++ tutorial/03a-foreign-keys.sql
@@ -1,3 +1,6 @@
+SET NOEXEC OFF; -- re-enable code execution for safety
+GO
+
 USE [sample];
 GO
 
@@ -11,34 +14,50 @@ PRINT 'INFO | Initial Database Context: ' + DB_NAME();
 GO
 
 /*
-    name of the foreign key
+************************************************************************************
+    name of the foreign key to use with OBJEC_ID()
     cannot use GO while @FK_NAME is defined
     after the GO the scope dies and with it the variable
+************************************************************************************
 */
 DECLARE @FK_NAME VARCHAR(128) = 'dbo.FK_tblPerson_tblGender';
-
+-- check existence of the FK
+;WITH fkid
+AS
+(
+    SELECT COALESCE(
+                /* 
+                    object_id return an integer, 
+                    so coalesce must use an integer too !!!
+                */
+                OBJECT_ID(@FK_NAME, 'F'),               -- if the foreign key does not exist returns null
+                0                                       -- if object_id returns null, returns 0
+            ) AS FK_ID
+)
+SELECT 
+    FK_ID,
+    CASE 
+        WHEN FK_ID = 0 THEN 'NOT_FOUND ' + @FK_NAME     -- no || concat operator
+        ELSE 'FOUND ' + @FK_NAME                        -- then and else must be of the same type
+    END AS 'FOUND FK_ID?'                               -- no dynamic strings for aliases
+FROM fkid
+;
 /*
 ************************************************************************************
-    check existence of the FK
-************************************************************************************
-*/
-PRINT COALESCE(
-            /* object_id return an integer, so coalesce must use an integer too */
-            OBJECT_ID(@FK_NAME, 'F'),   -- if the foreign key does not exist returns null
-            0                           -- if object_id returns null, returns 0
-    );
-
-/*
     oneliner
+************************************************************************************
 */
-SELECT OBJECT_ID(@FK_NAME, 'F');
+SELECT OBJECT_ID(@FK_NAME, 'F') AS 'oneliner';
+
 /*
+************************************************************************************
     composite statement
+************************************************************************************
 */
 DECLARE @FK_ID INT = OBJECT_ID(@FK_NAME, 'F');
 SELECT @FK_ID as FK_ID;
 
-PRINT COALESCE(
+SELECT COALESCE(
         CAST(
             OBJECT_ID(@FK_NAME, 'F') AS VARCHAR(20)
         ),
@@ -46,27 +65,31 @@ PRINT COALESCE(
             or use convert, only mssql
             CONVERT(VARCHAR(10), OBJECT_ID(@FK_NAME, 'F'))
         */
-        'not found'
-    );
+        'not found ' + @FK_NAME
+    ) AS 'composite statement'
+;
 
 /*
 ************************************************************************************
     create FK if it doesnt exist
 ************************************************************************************
 */
-IF OBJECT_ID(@FK_NAME, 'F') IS NULL
-    BEGIN
-        ALTER TABLE [dbo].[tblPerson]               -- object do modify
-        ADD CONSTRAINT [FK_tblPerson_tblGender]     -- name of constraint
-        FOREIGN KEY ([GenderId])                    -- foreign key column
-        REFERENCES [dbo].[tblGender] ([ID]);        -- external referenced column
-        
-        PRINT 'INFO | Foreign Key ' + @FK_NAME + ' created.';
-    END
-ELSE
-    BEGIN
-        PRINT 'INFO | Foreign Key ' + @FK_NAME + ' already exists.';
-    END
+IF OBJECT_ID(@FK_NAME, 'F') IS NOT NULL
+BEGIN
+    SELECT 'INFO | Foreign Key ' + @FK_NAME + ' already exists.' AS MESSAGE
+    UNION ALL
+    SELECT 'INFO | DROPPING Foreign Key ' + @FK_NAME AS MESSAGE;
+    ALTER TABLE [dbo].[tblPerson]
+    DROP CONSTRAINT [FK_tblPerson_tblGender];
+    SELECT 'INFO | Foreign Key ' + @FK_NAME + ' dropped.' AS MESSAGE;
+END
+SELECT 'INFO | CREATING Foreign Key ' + @FK_NAME AS MESSAGE;
+ALTER TABLE [dbo].[tblPerson]               -- object do modify
+ADD CONSTRAINT [FK_tblPerson_tblGender]     -- name of constraint
+FOREIGN KEY ([GenderId])                    -- foreign key column
+REFERENCES [dbo].[tblGender] ([ID]);        -- external referenced column
+
+SELECT 'INFO | Foreign Key ' + @FK_NAME + ' created.' AS MESSAGE;
 GO
 
 /* 
@@ -80,7 +103,9 @@ BEGIN TRY
     VALUES (1, 'name', 'email', 1);
 END TRY
 BEGIN CATCH
-    PRINT 'ERROR | An error occurred. Execution jumped to the CATCH block.';
-    SELECT ERROR_NUMBER(), ERROR_MESSAGE();
+    SELECT 'ERROR | An error occurred. Execution jumped to the CATCH block.' AS MESSAGE
+    UNION ALL
+    SELECT CAST(ERROR_NUMBER() AS NVARCHAR) + ' - ' + ERROR_MESSAGE() AS MESSAGE
 END CATCH
 GO
+SET NOEXEC ON;  -- stop execution like exit() in python
