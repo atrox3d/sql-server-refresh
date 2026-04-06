7dce92a326dfe29cb689474de8de888cad2f0749
atrox3d <atrox3d@gmail.com>
2026-04-06 06:03:57 +0000

    fixing gemini credentials

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -5,34 +5,39 @@ set -e
 
 
 
+CONTAINER_PROJECT_PATH="${PWD}"
+LOGFILE="${CONTAINER_PROJECT_PATH}"/on-create.log
 echo "Executing post-creation script..."
 # 2. Run the original Python environment setup
 # pipx install uv && uv venv --allow-existing && uv sync
 
 # 1. Restore Gemini credentials if they exist
-CONTAINER_PROJECT_PATH="${PWD}"
+{
+    CRED_SOURCE_DIR="${CONTAINER_PROJECT_PATH}/.devcontainer/.persisted-credentials"
+    CRED_SOURCE_FILE="${CRED_SOURCE_DIR}/"*.json
+    CRED_DEST_DIR="${HOME}/.gemini"
+    CRED_DEST_FILE="${CRED_DEST_DIR}/"*.json
 
-CRED_SOURCE_DIR="${CONTAINER_PROJECT_PATH}/.devcontainer/.persisted-credentials"
-CRED_SOURCE_FILE="${CRED_SOURCE_DIR}/oauth_creds.json"
-CRED_DEST_DIR="${HOME}/.gemini"
-CRED_DEST_FILE="${CRED_DEST_DIR}/oauth_creds.json"
-echo "checking for ${CRED_SOURCE_FILE}..."
-if [ -f "$CRED_SOURCE_FILE" ]; then
-    echo "Restoring Gemini credentials..."
-	echo "Creating credential dest dir ${CRED_DEST_DIT}..."
-    mkdir -p "$CRED_DEST_DIR"
-    echo "copying ${CRED_SOURCE_FILE} to ${CRED_DEST_FILE}..."
-	cp "$CRED_SOURCE_FILE" "$CRED_DEST_FILE"
-    # Ensure the 'vscode' user owns the restored files, not root
-	echo "chowing -R ${CRED_DESGT_DIR}"
-    chown -R vscode:vscode "${CRED_DEST_DIR}"
-	echo "Showing ${CRED_DEST_DIR}..."
-	ls -la "${CRED_DEST_DIR}"
-    echo "Credentials restored and permissions set."
-else
-    echo "ERROR | cannot find ${CRED_SOURCE_FILE}. cannot login into gemini"
-fi
-
+    echo -e "checking for: \n${CRED_SOURCE_FILE}\n..."
+    # if [ -f $CRED_SOURCE_FILE ]; then
+    if ls ${CRED_SOURCE_FILE} >/dev/null 2>&1; then
+        # echo "Files exist"
+        echo "Restoring Gemini credentials..."
+        echo "Creating credential dest dir ${CRED_DEST_DIT}..."
+        mkdir -p "$CRED_DEST_DIR"
+        echo "copying ${CRED_SOURCE_FILE} to ${CRED_DEST_FILE}..."
+        cp ${CRED_SOURCE_FILE} "${CRED_DEST_DIR}"
+        # Ensure the 'vscode' user owns the restored files, not root
+        echo "chowing -R ${CRED_DESGT_DIR}"
+        chown -R vscode:vscode "${CRED_DEST_DIR}"
+        echo "Showing ${CRED_DEST_DIR}..."
+        ls -la "${CRED_DEST_DIR}"
+        echo "Credentials restored and permissions set."
+    else
+        echo -e "ERROR | cannot find \n${CRED_SOURCE_FILE}\n cannot login into gemini"
+        read -p "press ENTER"
+    fi
+} 2>&1 | tee -a "${LOGFILE}"
 
 ALIASES_PATH="${CONTAINER_PROJECT_PATH}/.devcontainer/.aliases"
 BASHRC_PATH="${HOME}/.bashrc"
