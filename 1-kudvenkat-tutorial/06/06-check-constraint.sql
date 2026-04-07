884e443624510bf3b9f313841cf08186b6007caa
atrox3d <atrox3d@gmail.com>
2026-04-07 14:07:33 +0000

    check constraints

--- 1-kudvenkat-tutorial/06/06-check-constraint.sql
+++ 1-kudvenkat-tutorial/06/06-check-constraint.sql
@@ -13,13 +13,14 @@ GO
 SET NOCOUNT ON;
 
 SELECT DB_NAME() AS db_name;
-PRINT 'INFO | Initial Database Context: ' + DB_NAME();
+DECLARE @Msg NVARCHAR(MAX) = 'Initial Database Context: ' + DB_NAME();
+EXEC dbo.spInfo @Msg, 1;
 GO
 
 --*********************************
 --- prepare data
 --*********************************
-PRINT 'INFO | Executing sp_ResetDemoData...'
+EXEC dbo.spInfo 'Executing sp_ResetDemoData...', 1;
 EXEC dbo.sp_ResetDemoData;
 GO
 
@@ -27,7 +28,7 @@ IF OBJECT_ID('CK_tblPerson_Age', 'C') IS NOT NULL
     BEGIN
         ALTER TABLE tblPerson
         DROP CONSTRAINT CK_tblPerson_Age ;
-        PRINT 'INFO | dropping Check Constraint CK_tblPerson_Age created.';
+        EXEC dbo.spInfo 'dropping Check Constraint CK_tblPerson_Age created.', 1;
     END
 GO
 
@@ -35,19 +36,19 @@ IF COL_LENGTH('tblPerson', 'Age') IS NOT NULL
     BEGIN
         ALTER TABLE tblPerson
         DROP COLUMN Age;
-        PRINT 'INFO | Dropped Column Age form tblPerson.';
+        EXEC dbo.spInfo 'Dropped Column Age form tblPerson.', 1;
     END
 
 ALTER TABLE tblPerson
 ADD Age INT NULL;
-PRINT 'INFO | Column Age added to tblPerson.';
+EXEC dbo.spInfo 'Column Age added to tblPerson.', 1;
 GO
 
 SELECT * from tblPerson;
 GO
 
 
-PRINT 'INFO | adding wrong age before setting check constraint...';
+EXEC dbo.spInfo 'adding wrong age before setting check constraint...', 1;
 INSERT INTO tblPerson
 VALUES
 (6, 'wrongage', 'wrong@age.com', 1, -1000);
@@ -59,36 +60,37 @@ BEGIN
     WITH NOCHECK                        -- this does not fail checking the current records
     ADD CONSTRAINT CK_tblPerson_Age 
     CHECK (Age >= 0 AND Age < 150);
-    PRINT 'INFO | Check Constraint CK_tblPerson_Age created.';
+    EXEC dbo.spInfo 'Check Constraint CK_tblPerson_Age created.', 1;
 END
 GO
 
 -- Demonstrate TRY...CATCH to handle errors within a batch
 BEGIN TRY
-    PRINT 'INFO | Trying to add wrong age after setting check constraint...';
+    EXEC dbo.spInfo 'Trying to add wrong age after setting check constraint...', 1;
     INSERT INTO tblPerson VALUES (7, 'wrongage', 'wrong@age.com', 1, -1000);
-    PRINT 'INFO | This line will NOT be reached.';
+    EXEC dbo.spInfo 'This line will NOT be reached.', 1;
 END TRY
 BEGIN CATCH
-    PRINT 'ERROR | An error occurred. Execution jumped to the CATCH block.';
-    PRINT 'ERROR | Error Number: ' + CAST(ERROR_NUMBER() AS VARCHAR);
-    PRINT 'ERROR | Error Message: ' + ERROR_MESSAGE();
+    EXEC dbo.spError 'An error occurred. Execution jumped to the CATCH block.';
     -- To stop the entire script, we must re-throw the error.
     -- Because of ":ON ERROR EXIT" at the top, this will terminate the script.
     -- THROW; 
     -- Stop the script execution for subsequent batches
-    PRINT 'ERROR | Stopping script execution via SET NOEXEC ON.';
+    EXEC dbo.spInfo 'Stopping script execution via SET NOEXEC ON.', 1;
     SET NOEXEC ON;
 END CATCH
 GO
 
-PRINT 'INFO | This batch will NOT execute because the previous batch re-threw an error.';
-PRINT 'INFO | This batch will NOT execute because the previous batch set NOEXEC ON.';
-PRINT 'INFO | Adding null age. This will SUCCEED because CHECK constraints allow NULLs by default.';
+EXEC dbo.spInfo 'This batch will NOT execute because the previous batch re-threw an error.';
+EXEC dbo.spInfo 'This batch will NOT execute because the previous batch set NOEXEC ON.';
+EXEC dbo.spInfo 'Adding null age. This will SUCCEED because CHECK constraints allow NULLs by default.';
+EXEC dbo.spFLush;
+GO
+
 INSERT INTO tblPerson
 VALUES
 (8, 'nullage', 'null@age.com', 1, NULL);
-PRINT 'INFO | NULL value inserted successfully.';
+EXEC dbo.spInfo 'NULL value inserted successfully.', 1;
 GO
 
 SELECT * from tblPerson;
