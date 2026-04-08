f521cc816c71d3dd0fe8f4942fbec8284ffdecaf
atrox3d <atrox3d@gmail.com>
2026-04-08 10:58:26 +0000

    add explanations for unique constraint

--- 1-kudvenkat-tutorial/09-unique/09-unique.sql
+++ 1-kudvenkat-tutorial/09-unique/09-unique.sql
@@ -1,8 +1,12 @@
--- SQLCMD Mode command: Stop execution on any error in any batch.
--- This must be enabled in the client (e.g., SSMS Query -> SQLCMD Mode).
--- :ON ERROR EXIT
--- GO
--- Ensure we start with execution enabled (in case previous run stopped it)
+/*
+************************************************************************************
+    SQLCMD Mode command: Stop execution on any error in any batch.
+    This must be enabled in the client (e.g., SSMS Query -> SQLCMD Mode).
+    :ON ERROR EXIT
+    GO
+    Ensure we start with execution enabled (in case previous run stopped it)
+************************************************************************************
+*/
 SET NOEXEC OFF;
 GO
 
@@ -13,56 +17,74 @@ GO
 SET NOCOUNT ON;
 
 SELECT DB_NAME() AS db_name;
-PRINT 'INFO | Initial Database Context: ' + DB_NAME();
+DECLARE @Msg NVARCHAR(MAX) = 'Initial Database Context: ' + DB_NAME();
+EXEC dbo.spInfo @Msg, 1;
 GO
 
---****************************************************************
--- create constraint
---****************************************************************
+
+/*
+************************************************************************************
+    prepare data
+************************************************************************************
+*/
+TRUNCATE TABLE dbo.tblPerson;
+GO
+
+/*
+************************************************************************************
+    create constraint UQ_tblPerson_Email, requires the column email to be unique
+************************************************************************************
+*/
 IF OBJECT_ID('UQ_tblPerson_Email') IS NOT NULL
     BEGIN
         ALTER TABLE dbo.TblPerson
         DROP CONSTRAINT UQ_tblPerson_Email;
-        PRINT 'INFO | dropping Unique Constraint UQ_tblPerson_Email created.';
+        EXEC dbo.spInfo 'dropping Unique Constraint UQ_tblPerson_Email created.', 1;
     END
 GO
-
-ALTER TABLE dbo.TblPerson
-ADD CONSTRAINT UQ_tblPerson_Email 
-UNIQUE (Email);
+EXEC dbo.spInfo 're-creating Unique Constraint UQ_tblPerson_Email created.', 1;
+ALTER TABLE dbo.TblPerson           -- target table
+ADD CONSTRAINT UQ_tblPerson_Email   -- constraint name
+UNIQUE (Email);                     -- constraint type and column name
 GO
 
---****************************************************************
--- prepare data
---****************************************************************
-DELETE FROM dbo.tblPerson;
-GO
 
 INSERT INTO dbo.tblPerson
 (ID, name, email, genderid, age)
 VALUES (1, 'first', 'f@f.com', 1, 30)
 GO
 
---****************************************************************
--- try to add duplicate
---****************************************************************
-INSERT INTO dbo.tblPerson
-(ID, name, email, genderid, age)
-VALUES (2, 'second', 'f@f.com', 1, 30)
--- Violation of UNIQUE KEY constraint 'UQ_tblPerson_Email'. 
--- Cannot insert duplicate key in object 'dbo.tblPerson'. The duplicate key value is (f@f.com).
+/*
+************************************************************************************
+    try to add duplicate
+************************************************************************************
+*/
+BEGIN TRY
+    INSERT INTO dbo.tblPerson
+    (ID, name, email, genderid, age)
+    VALUES (2, 'second', 'f@f.com', 1, 30)
+END TRY
+BEGIN CATCH
+    EXEC dbo.spError 'An error occurred. Execution jumped to the CATCH block.', 1;
+    -- Violation of UNIQUE KEY constraint 'UQ_tblPerson_Email'. 
+    -- Cannot insert duplicate key in object 'dbo.tblPerson'. The duplicate key value is (f@f.com).
+END CATCH
 GO
 
---****************************************************************
--- remove unique
---****************************************************************
+/*
+************************************************************************************
+    remove unique
+************************************************************************************
+*/
 ALTER TABLE dbo.TblPerson
 DROP CONSTRAINT UQ_tblPerson_Email;
 GO
 
---****************************************************************
--- add duplicate
---****************************************************************
+/*
+************************************************************************************
+    add duplicate
+************************************************************************************
+*/
 INSERT INTO dbo.tblPerson
 (ID, name, email, genderid, age)
 VALUES (2, 'second', 'f@f.com', 1, 30)
@@ -71,16 +93,24 @@ GO
 SELECT * from dbo.tblPerson;
 GO
 
---****************************************************************
--- try to add unique constraint
---****************************************************************
-ALTER TABLE dbo.TblPerson
-ADD CONSTRAINT UQ_tblPerson_Email 
-UNIQUE (Email);
--- The CREATE UNIQUE INDEX statement terminated because a duplicate key was found 
--- for the object name 'dbo.tblPerson' and the index name 'UQ_tblPerson_Email'. 
--- The duplicate key value is (f@f.com).
+/*
+************************************************************************************
+    try to add unique constraint
+************************************************************************************
+*/
+BEGIN TRY
+    ALTER TABLE dbo.TblPerson
+    ADD CONSTRAINT UQ_tblPerson_Email 
+    UNIQUE (Email);
+END TRY
+BEGIN CATCH
+    EXEC dbo.spError 'An error occurred. Execution jumped to the CATCH block.', 1;
+    -- The CREATE UNIQUE INDEX statement terminated because a duplicate key was found 
+    -- for the object name 'dbo.tblPerson' and the index name 'UQ_tblPerson_Email'. 
+    -- The duplicate key value is (f@f.com).
+END CATCH
 GO
 
-SELECT OBJECT_ID('UQ_tblPerson_Email'); -- NULL
+/* constraint is not created because it failed in the previous batch */
+SELECT OBJECT_ID('UQ_tblPerson_Email') AS [OBJECT_ID('UQ_tblPerson_Email')]; -- NULL
 GO
